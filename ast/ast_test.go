286ef2622933947271bccf4f881dfe4ee29f91f6
magicshui <shuiyuzhe@gmail.com>
2017-05-01 23:32:46 +0800

    add infix parser

---
+++ ast/ast_test.go
@@ -0,0 +1,27 @@
+package ast
+
+import (
+	"github.com/magicshui/echo/token"
+	"testing"
+)
+
+func TestString(t *testing.T) {
+	program := &Program{
+		Statements: []Statement{
+			&LetStatement{
+				Token: token.Token{Type: token.LET, Literal: "let"},
+				Name: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "myVar"},
+					Value: "myVar",
+				},
+				Value: &Identifier{
+					Token: token.Token{Type: token.IDENT, Literal: "anotherVar"},
+					Value: "anotherVar",
+				},
+			},
+		},
+	}
+	if program.String() != "let myvar = anotherVar;" {
+		t.Errorf("program.String() wrong. got=%q", program.String())
+	}
+}
