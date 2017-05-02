5a88416e40a345d23d6958c132830b2f52435c13
magicshui <shuiyuzhe@gmail.com>
2017-05-03 04:06:11 +0800

    add parase inter

--- repl/repl.go
+++ repl/repl.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io"
 
+	"github.com/magicshui/echo/evaluator"
 	"github.com/magicshui/echo/lexer"
 	"github.com/magicshui/echo/parser"
 )
@@ -29,8 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
